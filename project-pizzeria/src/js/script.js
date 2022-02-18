88e5d0b1ef2b6fe5489128dece13b4afe72de08c
Joanna Bach <joannakatarzynabach@gmail.com>
2022-02-18 13:17:52 +0100

    Add initAction method to cart class

--- project-pizzeria/src/js/script.js
+++ project-pizzeria/src/js/script.js
@@ -128,10 +128,8 @@
       thisProduct.dom.formInputs = thisProduct.dom.form.querySelectorAll(select.all.formInputs);
       thisProduct.dom.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
       thisProduct.dom.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
-      console.log('!!!!!!',thisProduct.dom.priceElem)
       thisProduct.dom.imageWrapper = thisProduct.element.querySelector(select.menuProduct.imageWrapper);
       thisProduct.dom.amountWidgetElem = thisProduct.element.querySelector(select.menuProduct.amountWidget);
-      console.log('DOM',thisProduct.dom)
     }
 
     initAccordion(){
@@ -288,6 +286,37 @@
     }
   }
 
+  
+  class Cart {
+    constructor(element){
+      const thisCart = this;
+
+      thisCart.products = [];
+
+      thisCart.getElements(element);
+      thisCart.initActions();
+
+      console.log('new Cart', thisCart);
+    }
+
+    getElements(element){
+      const thisCart = this;
+
+      thisCart.dom = {};
+
+      thisCart.dom.wrapper = element;
+      thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(select.cart.toggleTrigger);
+    }
+
+    initActions() {
+      const thisCart = this;
+
+      thisCart.dom.toggleTrigger.addEventListener('click', function(){
+        thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
+      });
+    }
+  }
+
   const app = {
     initMenu: function (){
       const thisApp = this;
@@ -315,28 +344,17 @@
 
       thisApp.initData();
       thisApp.initMenu();
+      thisApp.initCart();
     },
+
+    initCart: function (){
+      const thisApp = this;
+
+      const cartElem = document.querySelector(select.containerOf.cart);
+      thisApp.cart = new Cart(cartElem);
+      console.log(thisApp.cart);
+    }
   };
 
   app.init();
-
-  class Cart {
-    constructor(element){
-      const thisCart = this;
-
-      thisCart.products = [];
-
-      thisCart.getElements(element);
-
-      console.log('new Cart', thisCart);
-    }
-
-    getElements(element){
-      const thisCart = this;
-
-      thisCart.dom = {};
-
-      thisCart.dom.wrapper = element;
-    }
-  }
 }
